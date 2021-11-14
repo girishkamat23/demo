29d960bf9ada3a09b4d54f85a847ced75a037451
Gaurish Kamat <34893664+gaurishkamat@users.noreply.github.com>
2021-11-14 21:44:28 +0530

    Updated code

--- src/App.js
+++ src/App.js
@@ -1,62 +1,26 @@
 // import logo from './logo.svg';
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import { ContactList as ContactList1 } from './components/ContactList';
-import { v4 as uuid } from 'uuid';
 import Modal from './components/Modal';
+import CounterButton from './playground/CounterButton';
+import CounterHeader from './playground/CounterHeader';
+import CounterHOC from './playground/CounterHOC';
+import ContactBook from './components/ContactBook';
+import Home from './components/Home';
+import AboutUs from './components/AboutUs';
+import { Route, Routes, BrowserRouter, Link, NavLink } from 'react-router-dom';
 //functional components
 //class components
 //JSX- JavaScript XML- syntactic sugar
 // create destory- lifecycle phases- mount, update, unmount
 
-class App extends React.Component{
+class App extends Component{
 
-  constructor(props) {
-    super(props);
-    console.log('Props', props);
-  }
-
-  state = {
-    counter: 0,
-    contacts: [
-      {
-        id: 1,
-        name: 'Moiez Shaikh',
-        address: 'Pune',
-        age: 28,
-        mobile: '9098927836'
-      },
-      {
-        id: 2,
-        name: 'Pooja Kamat',
-        address: 'Pune',
-        age: 28,
-        mobile: '7809826787'
-      },
-      {
-        id: 3,
-        name: 'Girish Kamat',
-        address: 'Pune',
-        age: 32,
-        mobile: '8937827836'
-      },
-      {
-        id: 4,
-        name: 'Siddhant Gorle',
-        address: 'Pune',
-        age: 21,
-        mobile: '9098098836'
-      }
-    ],
-    modal: {
-      name: '',
-      address: '',
-      age: '',
-      mobile: ''
-    },
-    showModal: false
-  }
-
+  // constructor(props) {
+  //   super(props);
+  // }
+  
   // static getDerivedStateFromProps() {
   //   console.log("getDerivedStateFromProps");
   //   console.log("getDerivedStateFromProps");
@@ -82,71 +46,6 @@ class App extends React.Component{
   //   this.setState(obj)
   // }
 
-  deleteContact = (id) => {
-    let newContacts = [...this.state.contacts];
-    
-    let index = newContacts.findIndex((item) => item.id === id);
-    
-    newContacts.splice(index, 1);
-    
-    this.setState({
-      contacts: newContacts
-    })
-  }
-  
-  addContact = () => {
-    let newContacts = [...this.state.contacts];
-
-    let contact = { ...this.state.modal, id: uuid() };
-
-    newContacts.push(contact);
-
-    this.setState({
-      contacts: newContacts
-    }, () => {
-      this.setState({
-        modal: {
-          name: '',
-          address: '',
-          age: '',
-          mobile: ''
-        },
-        showModal: false
-      })
-    })
-
-  }
-
-  //toggle this.showModal
-  setShowModal = () => {
-    this.setState({
-      showModal: !this.state.showModal,
-      modal: {
-        name: '',
-        address: '',
-        age: '',
-        mobile: ''
-      },
-    })
-  }
-
-  onChangeListener = (e) => {
-    let value = e.target.value;
-    let id = e.target.id;
-
-    let obj = {};
-    obj[id] = value;
-    
-    console.log(e)
-    this.setState({
-      modal: {
-        ...this.state.modal,
-        ...obj
-      }
-    })
-
-  }
-
   render() {
     return (
       <div className='container'>
@@ -156,26 +55,26 @@ class App extends React.Component{
           decrement={this.decrementCounter}
         /> */}
 
-        {console.log(this.state.contacts)}
+        
+      
+        <BrowserRouter>
+          <header>
+            <ul className='header-list'>
+              <NavLink to='/'  id='link' activeClassName='active'> Home </NavLink>
+              <NavLink to='/contactbook'   id='link' activeClassName='active'> Contact Book </NavLink>
+              <NavLink to='/aboutus'  id='link' activeClassName='active' > About Us </NavLink> 
+            </ul>
+          </header>
+            <Routes>
+              <Route path='/' element={<Home />}></Route>
+              <Route path='/contactbook' element={<ContactBook />}></Route>
+              <Route path='/aboutus' element={<AboutUs />}></Route>
+            </Routes>
+        </BrowserRouter>
 
-        <div className='action-header'>
-                <button onClick={this.setShowModal}>Add Contact</button>
-        </div>
-        <ContactList1
-          contacts = {this.state.contacts}
-          deleteContact = {this.deleteContact}
-          addContact = {this.addContact}
-        />
-        <Modal
-          showModal={this.state.showModal}
-          setShowModal={this.setShowModal}
-          name={this.state.modal.name}
-          address={this.state.modal.address}
-          age={this.state.modal.age}
-          mobile={this.state.modal.mobile}
-          onChangeListener={this.onChangeListener}
-          addContact={this.addContact}
-        />
+        {/* HOC */}
+        {/* <CounterButton></CounterButton>
+        <CounterHeader></CounterHeader> */}
       </div>
     )
   }
@@ -194,3 +93,9 @@ export default App;
 
 //props are immutable object
 //state can be mutated
+
+// Higher Order Components HOC- HOC is basically a function that takes in a Component and returns a new Component(Enhanced Component)
+
+// Routing
+
+
